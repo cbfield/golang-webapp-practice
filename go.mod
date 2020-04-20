b607b21461e504de84350244b8a6a650fc821eb3
Christopher Basketfield <chris.basketfield@gmail.com>
2020-04-19 22:39:42 -0800

    basic user authentication

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-redis/redis v6.15.7+incompatible
 	github.com/gorilla/mux v1.7.4
 	github.com/gorilla/sessions v1.2.0
+	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904
 )
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"text/template"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
@@ -25,7 +27,9 @@ func main() {
 
 	r.HandleFunc("/login", loginHandlerGet).Methods("GET")
 	r.HandleFunc("/login", loginHandlerPost).Methods("POST")
-	r.HandleFunc("/test", testHandler).Methods("GET")
+
+	r.HandleFunc("/register", registerHandlerGet).Methods("GET")
+	r.HandleFunc("/register", registerHandlerPost).Methods("POST")
 
 	r.HandleFunc("/home", homeHandlerGet).Methods("GET")
 	r.HandleFunc("/blog", blogHandlerGet).Methods("GET")
@@ -39,6 +43,12 @@ func main() {
 }
 
 func rootHandlerGet(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	_, ok := session.Values["username"]
+	if !ok {
+		http.Redirect(w, r, "/login", 302)
+	}
+
 	comments, err := client.LRange("comments", 0, 10).Result()
 	if err != nil {
 		return
@@ -60,22 +70,39 @@ func loginHandlerGet(w http.ResponseWriter, r *http.Request) {
 func loginHandlerPost(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+
+	hash, err := client.Get("user:" + username).Bytes()
+	if err != nil {
+		return
+	}
+	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
+	if err != nil {
+		return
+	}
+
 	session, _ := store.Get(r, "session")
 	session.Values["username"] = username
 	session.Save(r, w)
+	http.Redirect(w, r, "/", 302)
 }
 
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "session")
-	untyped, err := session.Values["username"]
-	if !err {
+func registerHandlerGet(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "register.html", nil)
+}
+
+func registerHandlerPost(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+
+	cost := bcrypt.DefaultCost
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	if err != nil {
 		return
 	}
-	username, err := untyped.(string)
-	if !err {
-		return
-	}
-	w.Write([]byte(username))
+	client.Set("user:"+username, hash, 0)
+	http.Redirect(w, r, "/login", 302)
 }
 
 func homeHandlerGet(w http.ResponseWriter, r *http.Request) {
