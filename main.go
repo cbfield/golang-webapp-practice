455d9b6ac02b1835d4f7e6a922d57a2f8867b304
Christopher Basketfield <chris.basketfield@gmail.com>
2020-04-20 21:30:30 -0800

    middleware

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ func main() {
 	client = redis.NewClient(&redis.Options{Addr: "localhost:6379"})
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", rootHandlerGet).Methods("GET")
-	r.HandleFunc("/", rootHandlerPost).Methods("POST")
+	r.HandleFunc("/", authRequired(rootHandlerGet)).Methods("GET")
+	r.HandleFunc("/", authRequired(rootHandlerPost)).Methods("POST")
 
 	r.HandleFunc("/login", loginHandlerGet).Methods("GET")
 	r.HandleFunc("/login", loginHandlerPost).Methods("POST")
@@ -36,15 +36,22 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func rootHandlerGet(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "session")
-	_, ok := session.Values["username"]
-	if !ok {
-		http.Redirect(w, r, "/login", 302)
+func authRequired(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "session")
+		_, ok := session.Values["username"]
+		if !ok {
+			http.Redirect(w, r, "/login", 302)
+		}
+		handler.ServeHTTP(w, r)
 	}
+}
 
+func rootHandlerGet(w http.ResponseWriter, r *http.Request) {
 	comments, err := client.LRange("comments", 0, 10).Result()
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
 		return
 	}
 	templates.ExecuteTemplate(w, "index.html", comments)
@@ -53,7 +60,12 @@ func rootHandlerGet(w http.ResponseWriter, r *http.Request) {
 func rootHandlerPost(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	comment := r.PostForm.Get("comment")
-	client.LPush("comments", comment)
+	err := client.LPush("comments", comment).Err()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
 	http.Redirect(w, r, "/", 302)
 }
 
@@ -67,11 +79,17 @@ func loginHandlerPost(w http.ResponseWriter, r *http.Request) {
 	password := r.PostForm.Get("password")
 
 	hash, err := client.Get("user:" + username).Bytes()
-	if err != nil {
+	if err == redis.Nil {
+		templates.ExecuteTemplate(w, "login.html", "Username Not Recognized")
+		return
+	} else if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
 		return
 	}
 	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
 	if err != nil {
+		templates.ExecuteTemplate(w, "login.html", "Incorrect Password")
 		return
 	}
 
@@ -93,8 +111,15 @@ func registerHandlerPost(w http.ResponseWriter, r *http.Request) {
 	cost := bcrypt.DefaultCost
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
 		return
 	}
-	client.Set("user:"+username, hash, 0)
+	err = client.Set("user:"+username, hash, 0).Err()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
 	http.Redirect(w, r, "/login", 302)
 }
